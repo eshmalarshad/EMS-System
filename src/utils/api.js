59273e8d07856ededa3c1ee57a117b73eb237023
Eshmal Arshad <eshmalsyeda@gmail.com>
2026-06-20 10:49:00 +0500

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ems-system-production-45bc.up.railway.app/api",
 });
 
 export default api;
