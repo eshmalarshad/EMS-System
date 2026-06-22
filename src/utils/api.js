805b2b98160639a41b3827d2633632fb7c1a4f28
Eshmal Arshad <eshmalsyeda@gmail.com>
2026-06-22 22:19:46 +0500

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://tender-wholeness-production-d6f4.up.railway.app/api",
 });
 
 export default api;
