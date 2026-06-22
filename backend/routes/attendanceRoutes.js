483d008ed2b245c4d06d3bdffd681ac6bc256ada
Eshmal Arshad <eshmalsyeda@gmail.com>
2026-06-22 14:00:18 +0500

    Update attendanceRoutes.js

--- backend/routes/attendanceRoutes.js
+++ backend/routes/attendanceRoutes.js
@@ -3,11 +3,17 @@ const router = express.Router();
 
 const {
   markAttendance,
+  clockIn,
+  clockOut,
   getAllAttendance,
   getMyAttendance,
 } = require("../controllers/attendanceController");
 
 const protect = require("../middleware/authMiddleware");
+
+// Employee/HR Clock-In & Clock-Out
+router.post("/clockin", protect, clockIn);
+router.post("/clockout", protect, clockOut);
 
 // Employee
 router.post("/mark", protect, markAttendance);
