789fd40dee8dd578cd312ee94e7fb9ad9683a06e
eshmalarshad <eshmalsyeda@gmail.com>
2026-06-22 22:00:56 +0500

    initial commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ems-system-production-45bc.up.railway.app/api",
+  baseURL: "http://localhost:5000/api",
 });
 
 export default api;
