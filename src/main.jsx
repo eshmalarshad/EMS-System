e9eb227e59d48f5bf5438bded48673078ee3d6b1
Eshmal Arshad <eshmalsyeda@gmail.com>
2026-06-23 19:55:51 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
+    <Toaster
+      position="top-right"
+      reverseOrder={false}
+    />
   </BrowserRouter>
 );
