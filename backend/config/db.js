c7d0f207a543ae772e130a09ff81b6d9bd938da9
Eshmal Arshad <eshmalsyeda@gmail.com>
2026-06-23 19:45:17 +0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+     await mongoose.connect(process.env.MONGO_URI, {
+      useFindAndModify: false,
+    });
     console.log("MongoDB Connected", mongoose.connection.name);
   } catch (error) {
     console.log("DB Error:", error.message);
